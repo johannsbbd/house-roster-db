3142bd0fd41b8576280b7ca7d7d72a0d3ce651bc
phindilem01046286 <phindilem01046286@antgroup.com>
2022-02-22 18:31:38 +0200

    refactored insert statements

--- Scripts/Setup/02_InitialData.sql
+++ Scripts/Setup/02_InitialData.sql
@@ -2,50 +2,50 @@
 INSERT INTO House
            (HouseName
            )
-VALUES    ('The Mays'), --1
-    ('Mkhize House'), --2
-    ('Chocho Crib'), --3
-    ('Schepers Home'), --4
-    ('Smiths'), --5
-    ('Terblanches Temple') --6
+VALUES    ('The Mays'), 
+    ('Mkhize House'), 
+    ('Chocho Crib'), 
+    ('Schepers Home'), 
+    ('Smiths'), 
+    ('Terblanches Temple') 
 GO
 
 -- Insert into person table
 INSERT INTO Person
            (FirstName
            ,LastName
-           ,PersonType -- varchar Gurdian / Normal
+           ,PersonType varchar GUARDIAN / NORMAL
            ,HouseId
            ) 
-VALUES    ('Thandile','May','Gurdian',1), --1
-    ('Thobelani','May', 'Gurdian',1), --2
-    ('Sindi','May', 'Normal',1), --3
-    ('Lwazi','May', 'Normal',1), --4
-    ('Thabo','May', 'Normal',1), --5
-    ('Sarah','Mkhize', 'Gurdian',2), --6
-    ('John','Mkhize', 'Normal',2), --7
-    ('Tim','Mkhize', 'Normal',2), --8
-    ('Tina','MKhize', 'Normal',2), --9
-    ('Nancy','Chocho', 'Normal',3),
-    ('Jim','Chocho', 'Normal',3),
-    ('Jeffery','Chocho', 'Gurdian',3),
-    ('Sihle','Chocho', 'Normal',3),
-    ('Toby','Chocho', 'Normal',3),
-    ('Tobey','Chocho', 'Normal',3),
-    ('Jennifer','Schepers', 'Gurdian',4),
-    ('Tom','Schepers', 'Gurdian',4),
-    ('Seth','Schepers', 'Normal',4),
-    ('Tony','Schepers', 'Normal',4),
-    ('Josh','Schepers', 'Normal',4),
-    ('Anne','Schepers', 'Normal',4),
-    ('Andrew','Smiths', 'Gurdian',5),
-    ('Martha','Smiths', 'Gurdian',5),
-    ('Kenneth','Smiths', 'Normal',5),
-    ('Kobey','Smiths', 'Normal',5),
-    ('Mary','Smiths', 'Normal',5),
-    ('Zama','Terblanche', 'Gurdian',6),
-    ('Tshepo','Terblanche', 'Normal',6),
-    ('Zoe','Terblanche', 'Normal',6)
+VALUES    ('Thandile','May','GUARDIAN',1), 
+    ('Thobelani','May', 'GUARDIAN',1), 
+    ('Sindi','May', 'NORMAL',1), 
+    ('Lwazi','May', 'NORMAL',1), 
+    ('Thabo','May', 'NORMAL',1), 
+    ('Sarah','Mkhize', 'GUARDIAN',2), 
+    ('John','Mkhize', 'NORMAL',2), 
+    ('Tim','Mkhize', 'NORMAL',2), 
+    ('Tina','MKhize', 'NORMAL',2), 
+    ('Nancy','Chocho', 'NORMAL',3),
+    ('Jim','Chocho', 'NORMAL',3),
+    ('Jeffery','Chocho', 'GUARDIAN',3),
+    ('Sihle','Chocho', 'NORMAL',3),
+    ('Toby','Chocho', 'NORMAL',3),
+    ('Tobey','Chocho', 'NORMAL',3),
+    ('Jennifer','Schepers', 'GUARDIAN',4),
+    ('Tom','Schepers', 'GUARDIAN',4),
+    ('Seth','Schepers', 'NORMAL',4),
+    ('Tony','Schepers', 'NORMAL',4),
+    ('Josh','Schepers', 'NORMAL',4),
+    ('Anne','Schepers', 'NORMAL',4),
+    ('Andrew','Smiths', 'GUARDIAN',5),
+    ('Martha','Smiths', 'GUARDIAN',5),
+    ('Kenneth','Smiths', 'NORMAL',5),
+    ('Kobey','Smiths', 'NORMAL',5),
+    ('Mary','Smiths', 'NORMAL',5),
+    ('Zama','Terblanche', 'GUARDIAN',6),
+    ('Tshepo','Terblanche', 'NORMAL',6),
+    ('Zoe','Terblanche', 'NORMAL',6)
 GO
 
 -- Insert into room table
@@ -53,34 +53,34 @@ INSERT INTO Room
            (RoomName
            ,HouseId
            )
-VALUES    ('Kitchen', 1), --1
-    ('Living Room', 1), --2
-    ('Bathroom', 1), --3
-    ('Main bedroom', 1), --4
-    ('Boys room', 1), --5
-    ('Sindis room', 1), --6
-    ('Main bedroom', 2), --7
-    ('Kids room', 2), --8
-    ('Kitchen', 2), --9
-    ('Lounge', 2), --10
-    ('Bathroom', 2) --11
+VALUES    ('Kitchen', 1), 
+    ('Living Room', 1), 
+    ('Bathroom', 1), 
+    ('Main bedroom', 1), 
+    ('Boys room', 1), 
+    ('Sindis room', 1), 
+    ('Main bedroom', 2), 
+    ('Kids room', 2), 
+    ('Kitchen', 2), 
+    ('Lounge', 2), 
+    ('Bathroom', 2) 
 GO
 
 -- Insert into task table
 INSERT INTO Task
            (RoomId
-           ,TaskDescription
+           ,TaskDesc
            )
-VALUES    (1,'mop the floors'), --1
-    (2,'Vaccum the couches'), --2
-    (3,'Wipe the mirror'), --3
-    (5, 'Put away toys'), --4
-    (6, 'fold clothes'), --5
-    (7,'just collect the laundry'), --6
-    (8,'fold blankets please'), --7
-    (9,'use one pod for the dishwasher!'), --8
-    (10, 'dust the shelves'), --9
-    (11, 'Main bathroom downstairs') --10
+VALUES    (1,'mop the floors'), 
+    (2,'Vaccum the couches'), 
+    (3,'Wipe the mirror'), 
+    (5, 'Put away toys'), 
+    (6, 'fold clothes'), 
+    (7,'just collect the laundry'), 
+    (8,'fold blankets please'), 
+    (9,'use one pod for the dishwasher!'), 
+    (10, 'dust the shelves'), 
+    (11, 'Main bathroom downstairs') 
 GO
 
 -- Insert into work  table
@@ -88,7 +88,7 @@ INSERT INTO Work
            (TaskId
            ,AssigneeId
            ,WorkDoneById
-           ,WorkStatus --TODO / DONE / FAILED
+           ,WorkStatus 
            ,DueDate
            ,DoneDate
            )
@@ -100,7 +100,7 @@ VALUES    (1, 3, 3, 'DONE', '2022-02-12', '2022-02-12'),
     (4, 5, NULL, 'TODO', '2022-03-22', NULL),
     (6, 7, 7, 'DONE', '2022-02-13', '2022-02-13'),
     (9, 7, 8, 'DONE', '2022-02-15', '2022-02-16'),
-    (11, 8, NULL, 'FAILED', '2022-02-03', NULL),
+    (8, 8, NULL, 'FAILED', '2022-02-03', NULL),
     (10, 9, NULL, 'TODO', '2022-03-20', NULL)
 GO
 
@@ -109,18 +109,18 @@ INSERT INTO TaskDay
            (TaskId
            ,TaskDay
            )
-VALUES    (1,'Monday'), --1
-    (2,'Tuesday'), --2
-    (2,'Wednesday'), --3
-    (5, 'Wednesday'), --4
-    (6, 'Thursday'), --5
-    (7,'Sunday'), --6
-    (8,'Saturday'), --7
-    (9,'Friday'), --8
-    (10, 'Monday'), --9
-    (11, 'Sunday') --10
-    (3,'Saturday'), --11
-    (8,'Friday'), --12
-    (6, 'Monday'), --13
-    (4, 'Sunday') --14
+VALUES    (1,'Monday'), 
+    (2,'Tuesday'), 
+    (2,'Wednesday'), 
+    (5, 'Wednesday'), 
+    (6, 'Thursday'), 
+    (7,'Sunday'), 
+    (8,'Saturday'), 
+    (9,'Friday'), 
+    (10, 'Monday'), 
+    (5, 'Sunday'), 
+    (3,'Saturday'), 
+    (8,'Friday'), 
+    (6, 'Monday'), 
+    (4, 'Sunday') 
 GO
