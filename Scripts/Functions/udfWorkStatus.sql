dab4d8790964136c7dde4695b6be6436be48c4ab
Xander Terblanche <xander.terblanche@vcontractor.co.za>
2022-02-24 13:29:26 +0200

    compile db scripts

--- Scripts/Functions/udfWorkStatus.sql
+++ Scripts/Functions/udfWorkStatus.sql
@@ -1,23 +1,23 @@
-CREATE FUNCTION udfWorkStatus
-(
-  @DueDate date, 
-  @DoneDate date,
-)
-RETURNS varchar(10)
-AS
-BEGIN
-  DECLARE @Status varchar
-  DECLARE @Now datetime set @Now = GETDATE()
-
-  SELECT @Status = CASE
-              WHEN @Now > @DueDate AND @DoneDate is null
-                  THEN 'FAILED'
-              WHEN @Now < @DueDate AND @DoneDate is null
-                    THEN 'TODO'
-              WHEN @DoneDate is not null and @DoneDate < @DueDate
-                    THEN 'COMPLETE'              
-              ELSE
-                     'LATE'
-  RETURN @Status
-END
+USE [HouseRoster]
 GO
+  /****** Object: UserDefinedFunction [dbo].[udfWorkStatus] Script Date: 2022/02/24 12:42:53 ******/
+SET
+  ANSI_NULLS ON
+GO
+SET
+  QUOTED_IDENTIFIER ON
+GO
+  ALTER FUNCTION [dbo].[udfWorkStatus] (@DueDate date, @DoneDate date) RETURNS varchar(10) AS BEGIN DECLARE @Status varchar(10) DECLARE @Now date
+set
+  @Now = GETDATE()
+SELECT
+  @Status = CASE
+    WHEN @Now > @DueDate
+    AND @DoneDate is null THEN 'FAILED'
+    WHEN @Now <= @DueDate
+    AND @DoneDate is null THEN 'TODO'
+    WHEN @DoneDate is not null
+    and @DoneDate <= @DueDate THEN 'COMPLETE'
+    ELSE 'LATE'
+  END RETURN @Status
+END
---
+++ Scripts/compile.py
@@ -0,0 +1,22 @@
+### Postgres SQL tables, with their properties:
+#
+# Person(id, firstName, lastName, type, house_id, available)
+# House(id, name)
+# Room(id, name, house_id)
+# Task(id, room_id, days)
+# Work(id,  assigned_person, completed_person, task_id, due_date, done_date)
+from pathlib import Path
+
+outputFile = 'setupdb.sql'
+
+def compileDump():    
+    fileDirs = ['./Setup', './Views', './Functions', './StoredProc']
+    with open(outputFile, 'w+') as outfile:
+        for directory in fileDirs:
+            for path in Path(directory).glob('**/*.sql'):
+                with open(path) as infile:
+                    for line in infile:
+                        print(line)
+                        outfile.write(line)
+
+compileDump()
